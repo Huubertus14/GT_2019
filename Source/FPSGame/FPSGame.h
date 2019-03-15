ac536c7b26faa1e6767a584624223dc3d896edd5
Huub van de Hoef <Huub_thomas@hotmail.com>
2019-03-15 20:27:02 +0100

    Minor changes made to backend

--- Source/FPSGame/FPSGame.h
+++ Source/FPSGame/FPSGame.h
@@ -3,3 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#ifndef __NETWORKSESSIONTEST_H__
+#define __NETWORKSESSIONTEST_H__
+
+#include "Engine.h"
+#include "UnrealNetwork.h"
+//#include "Online.h"
+#endif
