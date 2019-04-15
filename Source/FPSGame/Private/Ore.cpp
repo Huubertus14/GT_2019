5e3237ebc8d528cd024e5c6769b06e74e3229f4a
Huub van de Hoef <Huub_thomas@hotmail.com>
2019-04-15 11:27:10 +0200

    ADD Begin design of a level

--- Source/FPSGame/Private/Ore.cpp
+++ Source/FPSGame/Private/Ore.cpp
@@ -7,7 +7,7 @@ AOre::AOre()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	Life = 2;// FMath::RandRange(5, 11);
+	Life =  FMath::RandRange(13, 25);
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
 	RootComponent = MeshComp;
 	MeshComp->SetWorldScale3D(FVector(0.2f, 0.2f, 0.2f));
---
+++ enc_temp_folder/99bd93181ce697926e3c98a1931842/Ore.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "Ore.h"
+#include "UnrealNetwork.h"
+
+// Sets default values
+AOre::AOre()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Life = 2;// FMath::RandRange(5, 11);
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+	MeshComp->SetWorldScale3D(FVector(0.2f, 0.2f, 0.2f));
+
+	
+}
+
+// Called when the game starts or when spawned
+void AOre::BeginPlay()
+{
+	Super::BeginPlay();
+	SetReplicates(true);
+	
+}
+
+FVector AOre::OreDirection(FVector hitPoint) 
+{
+	FVector CenterPoint = GetActorLocation();
+	FVector OutDirection = hitPoint - CenterPoint;
+	return OutDirection;
+}
+
+void AOre::OreHitSpawn(FVector hitPoint) 
+{
+	if (Role == ROLE_Authority) {
+		FVector outwardVector = hitPoint;
+		outwardVector.Normalize(1.f);
+		Life--;
+		if (PickUpItem) {
+
+			FActorSpawnParameters spawnParams;
+			spawnParams.Owner = this;
+			FRotator rotator = FRotator(0.f, 0.f, 0.f);
+			FVector spawnLocation = hitPoint;
+			GetWorld()->SpawnActor<AResourcePickUpTrigger>(PickUpItem, spawnLocation, rotator ,spawnParams);
+		}
+
+		if (Life <= 0)
+		{
+			Destroy();
+		}
+	}
+}
+
